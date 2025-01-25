27766330ced9b46df54a3ed30f62077f97014311
Dominik Heiler <70112403+justwizhhh@users.noreply.github.com>
2025-01-25 00:02:06 +0000

    Basic player movement

--- Assets/Scripts/Common/PlayerController.cs
+++ Assets/Scripts/Common/PlayerController.cs
@@ -2,48 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
     // Main gameplay logic for player
 
+    // Public toggles
+    [Header("Movement Toggles")]
+    public float MaxMoveForce;
+    public float MaxSpeed;
+    public float MaxRotationDamping;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    [Space(10)]
+    [Header("Camera Toggles")]
+    public float MaxCameraSpeed;
+
+    // Private variables
+    private bool forwardInput;
+    private bool backwardInput;
+
+    // Object references
+    private Rigidbody rb;
+    private Camera cam;
+    private CinemachineVirtualCamera virtualCam;
+
+    private Vector2 lastMousePos;
+    private Vector2 currentMousePos;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        virtualCam = FindFirstObjectByType<CinemachineVirtualCamera>();
     }
 
-    // Input functions
-    void OnHorizontal(InputValue value)
+    private void Start()
     {
-        //moveSpeed.x = value.Get<Vector2>().x;
+        virtualCam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = MaxCameraSpeed;
+        virtualCam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = MaxCameraSpeed;
     }
 
-    void OnVertical(InputValue value)
-    {
-        //moveSpeed.y = value.Get<Vector2>().y;
-    }
-
-    void OnButton1(InputValue value)
-    {
-
-    }
-
-    void OnButton2(InputValue value)
-    {
-
-    }
-
-    void OnButton3(InputValue value)
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Basic forwards and backwards movement
+        forwardInput = Input.GetKey(KeyCode.W) ? true : false;
+        backwardInput = Input.GetKey(KeyCode.S) ? true : false;
+
+        if (forwardInput || backwardInput)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, virtualCam.transform.rotation, Time.deltaTime / MaxRotationDamping);
+            if (rb.velocity.sqrMagnitude <= MaxSpeed)
+            {
+                rb.AddForce(transform.forward * (forwardInput ? 1 : backwardInput ? -1 : 0) * MaxMoveForce, ForceMode.Acceleration);
+            }
+        }
     }
 }
